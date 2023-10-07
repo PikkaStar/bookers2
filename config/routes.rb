2d23bf3877cfb832c9051367b8b6e0bdca789653
PikkaStar <yokotaka1226@gmail.com>
2023-10-07 05:25:33 +0000

    day

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+ 
+  
+  get 'homes/about'=>'homes#about',as: 'about'
   get root to: 'homes#top'
   devise_for :users
+  resources :books,only: [:new,:create,:index,:show,:edit,:update,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
